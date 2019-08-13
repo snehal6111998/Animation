f27a9df221e7f54fb20c19a68a6dc98372078bd9
snehal6111998 <krantiprakash07@gmail.com>
2019-08-13 17:32:40 +0530

    animation assignment

---
+++ index.php
@@ -0,0 +1,43 @@
+<!Doctype html>
+<html>
+<head>
+	<title>Css leaves animation</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<section><br><br><br><br><br><br><br><br><br><br><br><br>
+		<h2>My Life My Rules....!</h2>
+	
+		<div class="set">
+			<div><img src="leave.png" height="30%" width="50%"></div>
+			<div><img src="leave3.png" height="30%" width="50%"></div>
+			<div><img src="leave2.png" height="30%" width="50%"></div>
+			<div><img src="leave.png" height="30%" width="50%"></div>
+			<div><img src="leave3.png" height="30%" width="50%"></div>
+			<div><img src="leave 2.png" height="30%" width="50%"></div>
+			
+			
+		</div>
+		<div class="set set2">
+			<div><img src="leave.png" height="30%" width="50%"></div>
+			<div><img src="leave3.png" height="30%" width="50%"></div>
+			<div><img src="leave2.png" height="30%" width="50%"></div>
+			<div><img src="leave.png" height="30%" width="50%"></div>
+			<div><img src="leave3.png" height="30%" width="50%"></div>
+			<div><img src="leave2.png" height="30%" width="50%"></div>
+			
+			
+		</div>
+		<div class="set set3">
+			<div><img src="leave.png" height="30%" width="50%"></div>
+			<div><img src="leave3.png" height="30%" width="50%"></div>
+			<div><img src="leave2.png" height="30%" width="50%"></div>
+			<div><img src="leave.png" height="30%" width="50%"></div>
+			<div><img src="leave3.png" height="30%" width="50%"></div>
+			<div><img src="leave2.png" height="30%" width="50%"></div>
+			
+			
+		</div>
+	</section>
+</body>
+</html>
